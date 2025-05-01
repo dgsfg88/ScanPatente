50e7918cd16d6f9a9d8a1b22c695510a39e10ed4
Andrea Ianni <andreaianni@hotmail.com>
2025-05-02 00:37:20 +0200

    #dev creato il ViewModel della MainWindow che attualmente permette di selezionare il dispositivo di acquisizione

---
+++ ScanPatente/ViewModel/ScanPatenteMainWindowModel.cs
@@ -0,0 +1,91 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using NAPS2.Scan;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanPatente.ViewModel
+{
+	internal partial class ScanPatenteMainWindowModel : ObservableObject, IDisposable
+	{
+		[ObservableProperty]
+		private ObservableCollection<ScanDevice> devices = new ObservableCollection<ScanDevice>();
+		[ObservableProperty]
+		private ScanDevice? selectedDevice = default;
+		private ScanningContext scanningContext;
+		private ScanController controller;
+		private CancellationTokenSource devicesUpdateCancellationToken;
+		private bool disposedValue;
+
+		public ScanPatenteMainWindowModel()
+		{
+			// Set up
+			scanningContext = new ScanningContext(new NAPS2.Images.Wpf.WpfImageContext());
+			controller = new ScanController(scanningContext);
+			devicesUpdateCancellationToken = new CancellationTokenSource();
+
+			Task.Run(async () => await PeriodicDeviceUpdateAsync(
+				new TimeSpan(0, 0, 0, 1), devicesUpdateCancellationToken.Token),
+				devicesUpdateCancellationToken.Token);
+		}
+
+		public async Task PeriodicDeviceUpdateAsync(TimeSpan interval, CancellationToken cancellationToken)
+		{
+			while (!cancellationToken.IsCancellationRequested)
+			{
+				// Query for available scanning devices
+				var devices = await controller.GetDeviceList();
+				if (devices != null)
+				{
+					var toRemove = Devices.Except(devices).ToList();
+					var toAdd = devices.Except(Devices).ToList();
+
+					foreach (var item in toRemove)
+						Devices.Remove(item);
+					foreach (var item in toAdd)
+						Devices.Add(item);
+
+					if (SelectedDevice is null)
+						SelectedDevice = Devices.FirstOrDefault();
+				}
+
+				await Task.Delay(interval, cancellationToken);
+			}
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+					devicesUpdateCancellationToken.Cancel();
+					devicesUpdateCancellationToken.Dispose();
+					scanningContext.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~ScanPatenteMainWindowModel()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
